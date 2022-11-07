a52db1d7731e5660a5004314a1cd03db0b118d50
Zakariya00 <100076090+Zakariya00@users.noreply.github.com>
2022-11-07 14:47:49 +0100

    Add files via upload

---
+++ Lab2/BlackJack.hs
@@ -0,0 +1,56 @@
+module BlackJack where
+
+import Cards 
+import RunGame 
+import Test.QuickCheck
+
+-- hand1 = Hand (Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty))
+
+-- A0 --
+{-
+size hand2
+  = size (Add (Card (Numeric 2) Hearts)
+              (Add (Card Jack Spades) Empty))
+  = 1 + size (Add (Card Jack Spades) Empty)
+  = 1 + 1 + size (Empty)
+  = 1 + 1 + 0
+  = 2
+-}
+
+-- A1 --
+displayCard :: Card -> String
+displayCard (Card r s)   = "" ++ show (r) ++ " of " ++ show (s)
+
+display :: Hand -> String
+display Empty            = ""
+display (Add card hand)  = displayCard (card) ++ ", " ++ display hand
+
+-- A2 --
+cardValue :: Card -> Integer -> Integer
+cardValue (Card (Numeric r)  _) (_)  = r
+cardValue (Card Ace _) (aceValue)  = aceValue
+cardValue (Card _ _) (_)           = 10
+
+initialValue :: Hand -> Integer -> Integer
+initialValue (Empty) (_)           = 0
+initialValue (Add card hand) (ace)  = cardValue (card) (ace) + initialValue (hand) (ace)
+
+value :: Hand -> Integer
+value hand =
+              case initialValue hand 11 > 21 of
+                             True -> initialValue hand 1
+                             False  -> initialValue hand 11
+
+-- A3 --
+gameOver :: Hand -> Bool
+gameOver hand = value hand > 21
+
+-- A4 --
+winner :: Hand -> Hand -> Player
+winner guest bank
+    | not (gameOver guest) && (gameOver bank)                  = Guest
+    | not (gameOver guest) && not (value guest <= value bank)  = Guest
+    | otherwise                                                = Bank
+
+
+
